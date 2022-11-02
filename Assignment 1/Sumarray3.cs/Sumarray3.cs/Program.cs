be7676c8a0ec856cc0d4eeac040f1c83cba589ac
AshwiniKallimath <97425201+AshwiniKallimath@users.noreply.github.com>
2022-11-02 11:21:46 +0530

    Update Program.cs

--- Assignment 1/Sumarray3.cs/Sumarray3.cs/Program.cs
+++ Assignment 1/Sumarray3.cs/Sumarray3.cs/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
+using System;
+#region
 public class Exercise3
 {
     private static void Sum()
-    {
+    {   #region FIELDS
         int i, n, sum = 0;
         Console.Write("Input the number of elements to be stored in the array :");
         n = Convert.ToInt32(Console.ReadLine());
         int[] a = new int[100];
+        #endregion
         for (i = 0; i < n; i++)
         {
             Console.Write("please enter the numbers :");
@@ -20,9 +22,11 @@ public class Exercise3
         Console.WriteLine("Sum of all elements stored in the array is : {0}\n\n", +sum);
         Console.ReadLine();
     }
-    public static void Main()
-    {
+    public static void Main(String[] args)
+    {  
+        #region BODY
         Exercise3.Sum();
         Console.ReadKey();
+        #endregion
     }
 }
